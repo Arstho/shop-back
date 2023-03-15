030556e31574e9edbcfa189c8a56a98797a98841
Yakumov <dgahar.yakumov@gmail.com>
2023-03-15 14:54:52 +0300

    init commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import fileUpload from 'express-fileupload'
 import authRoute from './routes/auth.js'
 import postRoute from './routes/posts.js'
 import commentRoute from './routes/comments.js'
-import catsRoute from './routes/categories.js'
+import catsRoute from './routes/categories.js'  
 
 const app = express()
 dotenv.config()
