d6069a236ee0569988b17a60d9e1fddf6a16b5ae
Aslan <asln86@mail.ru>
2023-03-16 12:01:59 +0300

    Добавил удаление пользователя

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -6,5 +6,6 @@ const authMiddleware = require("../midlewaree/auth.Midlewaree");
 router.post("/registration", controller.registration);
 router.post("/login", controller.login);
 router.get("/users", controller.getUsers);
+router.delete("/:user", controller.deleteUser);
 
 module.exports = router;
